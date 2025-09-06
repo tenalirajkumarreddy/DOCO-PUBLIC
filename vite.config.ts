656acd432864befd9e1f97d300781dd7c0fc6aa2
Raj Kumar Reddy <tech.razzukwn@gmail.com>
2025-09-06 21:07:36 +0530

    j

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false,
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name]-[hash].js',
@@ -17,5 +19,9 @@ export default defineConfig({
         assetFileNames: 'assets/[name]-[hash].[ext]'
       }
     }
+  },
+  server: {
+    host: true,
+    port: 3000
   }
 });
